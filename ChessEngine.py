e076f05547846a6b34c090f58f206d4daaa85d7f
Mufaddal Naguthanawala <m.nguthana@hotmail.com>
2024-08-31 16:18:18 -0400

    Draw board and pieces

---
+++ ChessEngine.py
@@ -0,0 +1,20 @@
+"""
+Store information about current state of chess game
+Dertermine valid moves at current state
+Maintain move log
+"""
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+        self.whiteToMove = True
+        self.movelog = []
