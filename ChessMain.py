b0cc5638bc7dede498a1005f21614785028caace
Mufaddal Naguthanawala <m.nguthana@hotmail.com>
2024-09-02 20:14:14 -0400

    fix piece selection bug

--- ChessMain.py
+++ ChessMain.py
@@ -56,8 +56,10 @@ def main():
                     if move in validMoves:
                         gs.makeMove(move)
                         moveMade = True
-                    sqSelected = ()
-                    playerClicks = []
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        playerClicks = []
             # Key handler
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
